e425d90c2e99fd84ff2d7fe8d1145ae1befbba83
Vijay Wankhede <142470931+VijayWankhede555@users.noreply.github.com>
2026-08-15 17:08:15 +0530

    Update 10 sort array.c

--- Programming In C/assignment08/10 sort array.c
+++ Programming In C/assignment08/10 sort array.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
 void main()
 {
-	int arr[4],arr1[7],arr2[30];
-	int i=arr[4];
-	int j=arr1[7];
-	int k=arr2[30];
+	int arr[5],i,j;
+	printf("enter number: ");
 	for(i=0;i<5;i++)
 	{
 		scanf("%d",&arr[i]);
-		for(i=0,k=0,i<5,k<5;i++,K++)
-	     arr[k]=arr[i];
-		printf("arr[k]%d",arr[k]);
 	}
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<4-i;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+			  int temp;
+			  temp=arr[j];
+			  arr[j]=arr[j+1];
+			  arr[j+1]=temp;	
+			}
+		}
+	}
+	for(i=0;i<5;i++)
+	{
+		printf("%d",arr[i]);
+	}
+	
 }
